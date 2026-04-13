9cb768f8e53ba7449cf765780320e8acec67082c
jonmolinos <molinosjon@gmail.com>
2026-04-13 23:27:17 +0200

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{padding: 40, fontFamily: "sans-serif"}}>
-      <h1>Klaizen 🚀</h1>
-      <p>Tu plataforma está desplegada correctamente</p>
-    </div>
-  );
-}
